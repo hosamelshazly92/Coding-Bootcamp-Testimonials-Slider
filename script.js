90099c7264595e84510ea822d2438d30e16d40f5
hosam-pc <hosamelshazly92@gmail.com>
2020-06-22 15:41:44 +0200

    merge branches & add previous button script

--- script.js
+++ script.js
@@ -26,3 +26,29 @@ next.addEventListener("click", function () {
     job1.style.display = "none";
     job2.style.display = "inline-block";
 });
+
+previous.addEventListener("click", function () {
+    let image1 = document.querySelector(".image-1"),
+        image2 = document.querySelector(".image-2");
+
+    image1.style.display = "inline-block";
+    image2.style.display = "none";
+
+    let main1 = document.querySelector("#main-1"),
+        main2 = document.querySelector("#main-2");
+
+    main1.style.display = "block";
+    main2.style.display = "none";
+
+    let name1 = document.querySelector("#name-1"),
+        name2 = document.querySelector("#name-2");
+
+    name1.style.display = "inline-block";
+    name2.style.display = "none";
+
+    let job1 = document.querySelector("#job-1"),
+        job2 = document.querySelector("#job-2");
+
+    job1.style.display = "inline-block";
+    job2.style.display = "none";
+});
